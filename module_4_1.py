ea5473785e06b28a4c60ece23baee01599eab950
shaaba762x <shaaba777@gmail.com>
2024-11-10 12:01:46 +0300

    m4_1

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fk_m
+from true_math import divide as fk_t
+
+
+
+result1 = fk_m(69, 3)
+result2 = fk_m(73, 0)
+result3 = fk_t(8, 4)
+result4 = fk_t(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
